06c259b970613ff4944733250104b63b1759fad1
Felipe Rivas <rivascf@gmail.com>
2024-11-01 19:14:17 -0600

    Updated sysinfo.py script Changes include: Create on function to execute shell commands. More efficient report's parts creation Change redundat process

--- arrg_utils/sysinfo.py
+++ arrg_utils/sysinfo.py
@@ -22,6 +22,7 @@ import subprocess
 import json
 import re
 import platform
+from typing import List, Dict, Union, Optional
 
 
 class SysInfo:
@@ -49,23 +50,34 @@ class SysInfo:
         )
         self.__disk_info = 'df -h | awk \'$NF=="/"{printf "%.1f|%.1f|%.1f", $2,$3,$4}\''
 
-    def get_host_info(self):
+    def __execute_command(self, command: str) -> Optional[str]:
+        """Executes a shell command and returns its output as a decoded string.
+
+        Args:
+            command (str): The shell command to execute.
+
+        Returns:
+            Optional[str]: The command output as a decoded string if successful, None otherwise.
+        """
+        try:
+            result = subprocess.check_output(command, shell=True)
+            return result.decode().strip()
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing command '{command}': {e}")
+            return None
+
+    def get_host_info(self) -> Dict[str, str]:
         """
         Retrieves the hostname and IP address of the system.
 
         Returns:
             dict: A dictionary containing `name` and `ip` keys with corresponding values.
         """
-        cmd = self.__host_ip
-        curr_ip = subprocess.check_output(cmd, shell=True)
-        str_ip = str(curr_ip).lstrip("b'")
-        str_ip = str_ip.rstrip("\\n'")
-        cmd = self.__host_name
-        host_cmd_out = subprocess.check_output(cmd, shell=True)
-        host_name = str(host_cmd_out).lstrip("b'").rstrip("\\n'")
+        str_ip = self.__execute_command(self.__host_ip) or ""
+        host_name = self.__execute_command(self.__host_name) or ""
         return {"name": host_name, "ip": str_ip}
 
-    def __parse_platform(self):
+    def __parse_platform(self) -> Dict[str, Union[str, Dict[str, str]]]:
         """
         Retrieves detailed OS information.
 
@@ -112,7 +124,7 @@ class SysInfo:
             "additional_info": additional_info,
         }
 
-    def get_free_disk(self):
+    def get_free_disk(self) -> Dict[str, float]:
         """
         Retrieves the total, used, and available disk space on the root directory.
 
@@ -120,49 +132,49 @@ class SysInfo:
             dict: A dictionary containing `size`, `used`, and `available`
             keys with corresponding values in GB.
         """
-        cmd = self.__disk_info
-        disk_cmd_output = subprocess.check_output(cmd, shell=True)
-        disk_info = str(disk_cmd_output).lstrip("b'")
-        disk_info = disk_info.rstrip("'")
-        disk_vals = disk_info.split("|")
-        return {
-            "size": float(disk_vals[0]),
-            "used": float(disk_vals[1]),
-            "available": float(disk_vals[2]),
-        }
+        disk_info = self.__execute_command(self.__disk_info)
+        if disk_info:
+            disk_vals = disk_info.split("|")
+            return {
+                "size": float(disk_vals[0]),
+                "used": float(disk_vals[1]),
+                "available": float(disk_vals[2]),
+            }
 
-    def get_free_ram(self):
+        return {"size": 0.0, "used": 0.0, "available": 0.0}
+
+    def get_free_ram(self) -> Dict[str, float]:
         """
         Retrieves the RAM statistics, including total, used, free, and available memory.
 
         Returns:
             dict: A dictionary containing `total`, `used`, `free`, and `available` keys.
         """
-        cmd = self.__ram_info
-        ram_cmd_output = subprocess.check_output(cmd, shell=True)
-        ram_info = str(ram_cmd_output).lstrip("b'")
-        ram_info = ram_info.rstrip("'")
-        ram_vals = ram_info.split("|")
-        return {
-            "total": float(ram_vals[0]),
-            "used": float(ram_vals[1]),
-            "free": float(ram_vals[2]),
-            "available": float(ram_vals[3]),
-        }
+        ram_info = self.__execute_command(self.__ram_info)
+        if ram_info:
+            ram_vals = ram_info.split("|")
+            return {
+                "total": float(ram_vals[0]),
+                "used": float(ram_vals[1]),
+                "free": float(ram_vals[2]),
+                "available": float(ram_vals[3]),
+            }
 
-    def get_system_date(self):
+        return {"total": 0.0, "used": 0.0, "free": 0.0, "available": 0.0}
+
+    def get_system_date(self) -> Dict[str, str]:
         """
         Retrieves the current system date and time.
 
         Returns:
             dict: A dictionary containing `date` and `time` keys.
         """
-        cmd = self.__sys_date
-        date_cmd_out = subprocess.check_output(cmd, shell=True)
-        date_time = str(date_cmd_out).lstrip("b'")
-        date_time = date_time.rstrip("\\n'")
-        date_parts = date_time.split("|")
-        return {"date": date_parts[0], "time": date_parts[1]}
+        date_time = self.__execute_command(self.__sys_date)
+        if date_time:
+            date_parts = date_time.split("|")
+            return {"date": date_parts[0], "time": date_parts[1]}
+
+        return {"date": "", "time": ""}
 
     def __compute_cpu_usage(
         self, user_val: int, system_val: int, idle_val: int
@@ -180,65 +192,58 @@ class SysInfo:
         """
         return float(user_val + system_val) * 100 / (user_val + system_val + idle_val)
 
-    def __full_cpu_stats(self):
+    def __full_cpu_stats(self) -> List[Dict[str, Union[int, float, str]]]:
         """
         Retrieves detailed CPU statistics for each core.
 
         Returns:
             list: A list of dictionaries containing detailed CPU statistics for each core.
         """
-        cpu_cmd_exec = subprocess.check_output(self.__cpu_stats_info, shell=True)
+        cpu_cmd_exec = self.__execute_command(self.__cpu_stats_info)
         cpu_parsed = str(cpu_cmd_exec).lstrip("b'").rstrip("@'")
-        cpu_array = cpu_parsed.split("@")
         cpu_stats = []
-        indx = 0
-        for cpu_data in cpu_array:
-            core_info = cpu_data.split("|")
-            user_val = int(core_info[1])
-            nice_val = int(core_info[2])
-            system_val = int(core_info[3])
-            idle_val = int(core_info[4])
-            perc_usaged = self.__compute_cpu_usage(
-                user_val=user_val, system_val=system_val, idle_val=idle_val
-            )
-            cpu_type = "total" if indx == 0 else "core"
-            cpu_stats.append(
-                {
-                    "id": indx,
-                    "label": core_info[0],
-                    "type": cpu_type,
-                    "core": -1 if indx == 0 else (indx - 1),
-                    "user": user_val,
-                    "nice:": nice_val,
-                    "system": system_val,
-                    "idle": idle_val,
-                    "usaged": perc_usaged,
-                }
-            )
-            indx += 1
+        if cpu_cmd_exec:
+            cpu_array = cpu_parsed.split("@")
+            for indx, cpu_data in enumerate(cpu_array):
+                core_info = cpu_data.split("|")
+                user_val = int(core_info[1])
+                nice_val = int(core_info[2])
+                system_val = int(core_info[3])
+                idle_val = int(core_info[4])
+                perc_usaged = self.__compute_cpu_usage(
+                    user_val=user_val, system_val=system_val, idle_val=idle_val
+                )
+                cpu_type = "total" if indx == 0 else "core"
+                cpu_stats.append(
+                    {
+                        "id": indx,
+                        "label": core_info[0],
+                        "type": cpu_type,
+                        "core": -1 if indx == 0 else (indx - 1),
+                        "user": user_val,
+                        "nice:": nice_val,
+                        "system": system_val,
+                        "idle": idle_val,
+                        "usaged": perc_usaged,
+                    }
+                )
 
         return cpu_stats
 
-    def __short_cpu_stats(self):
+    def __short_cpu_stats(self) -> List[Dict[str, Union[int, float, str]]]:
         """
         Retrieves a summary of the overall CPU usage.
 
         Returns:
             list: A list containing a dictionary with a single CPU usage percentage.
         """
-        cpu_cmd_exec = subprocess.check_output(self.__cpu_usage_short, shell=True)
-        cpu_parsed = str(cpu_cmd_exec).lstrip("b'").rstrip("\\n'")
-        cpu_stats = []
-        cpu_stats.append(
-            {
-                "id": 0,
-                "usaged": float(cpu_parsed),
-            }
-        )
+        cpu_usage = self.__execute_command(self.__cpu_usage_short)
 
-        return cpu_stats
+        return [{"id": 0, "usaged": float(cpu_usage)}] if cpu_usage else []
 
-    def get_cpu_usage(self, compute_value_only=False):
+    def get_cpu_usage(
+        self, compute_value_only=False
+    ) -> List[Dict[str, Union[int, float, str]]]:
         """
         Retrieves CPU usage information, either full stats or a summary.
 
@@ -249,13 +254,10 @@ class SysInfo:
         Returns:
             list: CPU usage information as a list of dictionaries.
         """
-        cpu_response = (
-            self.__full_cpu_stats()
-            if not compute_value_only
-            else self.__short_cpu_stats()
-        )
 
-        return cpu_response
+        return (
+            self.__short_cpu_stats() if compute_value_only else self.__full_cpu_stats()
+        )
 
     def __validate_ip(self, ipaddress: str) -> bool:
         """
@@ -267,13 +269,11 @@ class SysInfo:
         Returns:
             bool: True if the IP address is valid, False otherwise.
         """
-        regex_rule = re.compile(self.__ip_regex_pattern)
-        if re.search(regex_rule, ipaddress):
-            return True
+        return bool(re.match(self.__ip_regex_pattern, ipaddress))
 
-        return False
-
-    def parse_network_interfaces(self, filtered=True, target_ip=""):
+    def parse_network_interfaces(
+        self, filtered=True, target_ip: str = ""
+    ) -> List[Dict[str, Union[str, int, dict]]]:
         """
         Parses and retrieves network interfaces with optional filtering by exclusions and target IP.
 
@@ -284,29 +284,20 @@ class SysInfo:
         Returns:
             list: A list of dictionaries with network interface information.
         """
-        cmd = self.__ip_info
-        ip_data = subprocess.check_output(cmd, shell=True)
-        ip_info = json.loads(ip_data)
+        ip_data = self.__execute_command(self.__ip_info)
         ip_parsed = []
-        if target_ip != "":
-            if not self.__validate_ip(target_ip):
-                return ip_parsed.append(
-                    {"Error": f"Unabled to parse {target_ip} is not valid."}
-                )
-
-        for ip_item in ip_info:
-            ifname = ip_item["ifname"]
-            if not filtered:
-                ip_parsed.append(ip_item)
-            else:
-                if ifname not in self.__exclusions:
+        if ip_data:
+            ip_info = json.loads(ip_data)
+            for ip_item in ip_info:
+                ifname = ip_item["ifname"]
+                if not filtered or ifname not in self.__exclusions:
                     localip = ip_item["addr_info"][0]["local"]
                     if target_ip in ("", localip):
                         ip_parsed.append(ip_item)
 
         return ip_parsed
 
-    def get_ros_info(self):
+    def get_ros_info(self) -> Optional[Dict[str, Union[int, str, bool]]]:
         """
         Retrieves the ROS (Robot Operating System) environment details.
 
@@ -317,18 +308,14 @@ class SysInfo:
         if not ros_version:
             return None
 
-        ros_distro = os.environ.get("ROS_DISTRO")
-        ros_domain_id = os.environ.get("ROS_DOMAIN_ID")
-        ros_localhost = not os.environ.get("ROS_LOCALHOST_ONLY") == "0"
-
         return {
             "version": int(ros_version),
-            "distro": ros_distro,
-            "domain_id": int(ros_domain_id) if ros_domain_id else -1,
-            "localhost_only": ros_localhost,
+            "distro": os.environ.get("ROS_DISTRO", ""),
+            "domain_id": int(os.environ.get("ROS_DOMAIN_ID", -1)),
+            "localhost_only": os.environ.get("ROS_LOCALHOST_ONLY") != "0",
         }
 
-    def get_system_report(self):
+    def get_system_report(self) -> Dict[str, Union[str, dict, list]]:
         """
         Generates a comprehensive system report including:
             host, platform, CPU, RAM, disk, and network information.
@@ -345,7 +332,7 @@ class SysInfo:
         network_interfaces = self.parse_network_interfaces()
         ros_env = self.get_ros_info()
 
-        sys_report = {
+        return {
             "host": host_data["name"],
             "platform": os_data,
             "ip": host_data["ip"],
@@ -358,9 +345,7 @@ class SysInfo:
             "ros": ros_env if ros_env else "No ROS enviroment",
         }
 
-        return sys_report
-
-    def get_system_snapshot(self):
+    def get_system_snapshot(self) -> Dict[str, Union[str, dict, float]]:
         """
         Generates a snapshot of the system's current CPU, RAM, disk usage, and IP address.
 
